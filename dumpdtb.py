cbe68a36924597f16c47e1598af92c643c97d041
Ruslan Shymkevych <ruslan_shymkevych@epam.com>
2025-01-21 13:35:44 +0200

    dumbdtb: Fix UnicodeDecodeError related to device-tree parsing
    Fix error `UnicodeDecodeError: 'utf-8' codec can't decode byte
    0xd0 in position 0: invalid continuation byte`.
    
    Fixes: 9ed89a4a8c93 ("Fix UnicodeDecodeError related to device-tree parsing")
    
    Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- dumpdtb.py
+++ dumpdtb.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Dumb dtb')
     parser.add_argument('in_filename', help="input filename")
     args = parser.parse_args()
-    with open(args.in_filename) as infile:
+    with open(args.in_filename, 'rb') as infile:
         print FdtBlobParse(infile).to_fdt().to_dts()
